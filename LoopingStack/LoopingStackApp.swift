60dadc020d5a9d977688f073c7ea5b11ba3d8587
Taras <45391115+prystupataras@users.noreply.github.com>
2024-12-20 16:04:40 +0200

    Initial Commit

---
+++ LoopingStack/LoopingStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopingStackApp.swift
+//  LoopingStack
+//
+//  Created by Taras Prystupa on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoopingStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
